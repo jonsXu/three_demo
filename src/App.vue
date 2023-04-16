04819fcc424a2fae192bf126b6519784c294b10c
xujingwei <1198513700@qq.com>
2023-04-16 16:56:48 +0800

    add:新增项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import demo from './components/demo2.vue'
+</script>
+
+<template>
+  <div  style="width:100%;height:100vh">
+    <demo/>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/demo1.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref,onMounted } from 'vue'
+import * as THREE from 'three'
+// 轨道控制器
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+
+onMounted(() => {
+  let demo = document.getElementById('demo')
+  let width = demo.clientWidth
+  let height = demo.clientHeight
+  // 创建场景
+  const scene = new THREE.Scene();
+  // 创建相机
+  const camera = new THREE.PerspectiveCamera(75,width/height,0.1,1000)
+  // 设置相机的坐标
+  camera.position.set(0,0,10)
+  // 相机添加入场景
+  scene.add(camera)
+  // 创建几何体
+  const geometry = new THREE.BoxGeometry(2,2,2)
+  // 材质
+  const mesh = new THREE.MeshBasicMaterial({color: 0x00ff00})
+  // 基于几何体和材质创建立方体
+  const cube = new THREE.Mesh(geometry, mesh)
+  scene.add(cube)
+  
+  //初始化渲染器
+  const render = new THREE.WebGLRenderer()
+  // 设置渲染的尺寸和大小
+  render.setSize(width, height)
+
+  
+  //添加到body
+  demo.appendChild(render.domElement)
+
+  // 使用渲染器，通过相机将场景渲染
+  render.render(scene,camera)
+})
+
+</script>
+
+<template>
+  <div  style="width:100%;height:100%">
+    <div id="demo"  style="width:100%;height:100%"></div>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
